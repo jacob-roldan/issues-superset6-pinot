9922b1de8a8cdcef3f6b73e39cf193483ad0ef76
jacob-roldan <jacob.roldan@hpe.com>
2025-09-24 19:25:10 +0200

    Remove unnecessary dependencies from docker-compose and clean up birthnames script

--- example_birthnames.sh
+++ example_birthnames.sh
@@ -5,9 +5,7 @@ docker exec -ti pinot_db bash -c "sh ./examples/import-birthnames.sh"
 
 # Imports dashboards
 cd examples/birthnames
-zip -r dashboard_birthnames.zip dashboard/
 docker exec -ti superset_app bash -c "superset import-dashboards --path /app/docker/examples/birthnames/dashboard_birthnames.zip --username admin"
-
 
 # Open the browser for Pinot and Superset
 #open http://localhost:9000
